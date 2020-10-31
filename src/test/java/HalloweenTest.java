8df432681ed936db6b829182b1299b4972da361a
Hassan <zh_mosavi@hotmail.com>
2020-10-31 12:42:55 -0400

    no treat or trick

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,5 +2,11 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.printf("Hello World");
         // I am about to give everyone candy for halloween.
+        // are we thee yet??? Hello Hello
+        // lets play a gme
+        // its fun to drive so far
+        // there will be no treat and trick this year
+        // stay home and be safe and wear your mask outside
+
     }
 }
